9c94509fe5b30b4261571ac9c60b64a8a919561c
Nathan Estrada <naestrada19@gmail.com>
2019-08-12 11:50:27 -0700

    Branch off jpeg code

--- bot/cogs/context.py
+++ bot/cogs/context.py
@@ -64,47 +64,6 @@ class Context(commands.Cog):
             await channel.send("@everyone", embed=self.embed_image("https://i.imgur.com/Ca6EuUP.png"))
             await message.delete()
 
-        # Takes previous image in chat and compresses it
-        if content.lower().startswith("needsmorejpeg") or content.lower().startswith("needs more jpeg") \
-                or content.lower().startswith("morejpeg") or content.lower().startswith("more jpeg"):
-            number = 2
-            img = ""
-
-            # Get link to previous image in chat
-            async for x in channel.history(limit=number):
-                if x.content != "needsmorejpeg" or x.content != "needs more jpeg" or x.content != "morejpeg" \
-                        or x.content != "more jpeg":
-                    if x.content == "":
-                        img = x.attachments[0]["url"]
-                    else:
-                        img = x.content
-
-            # Download image locally to server
-            try:
-                async with aiohttp.ClientSession() as session:
-                    async with session.get(img) as resp:
-                        if resp.status == 200:
-                            file = await aiofiles.open("morejpeg.jpeg", mode="wb")
-                            await file.write(await resp.read())
-                            await file.close()
-
-                            # Save as JPEG in lowest quality and send it
-                            im = Image.open("morejpeg.jpeg")
-                            im = im.convert("RGB")
-                            im.save("morejpeg.jpeg", format="jpeg", quality=1)
-                            await channel.send(file=discord.File("morejpeg.jpeg"))
-
-                    # Delete off server
-                    os.remove("morejpeg.jpeg")
-            except Exception as e:
-                print(e)
-
-                if img.split(".")[1] == "gif":
-                    error = "Gif files are not supported"
-                else:
-                    error = "No image found in previous message"
-
-                await channel.send("```"+error+"```")
 
         # Deep fry previous image
         if content.lower().startswith("deepfry") or content.lower().startswith("deep fry"):
