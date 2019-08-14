8d5458e4427c583a7306269f4214bd6e1e8ea98c
Nathan Estrada <naestrada19@gmail.com>
2019-08-13 23:15:36 -0700

    Update deep_fry.py

--- bot/cogs/deep_fry.py
+++ bot/cogs/deep_fry.py
@@ -34,44 +34,40 @@ class DeepFry(commands.Cog):
             ext = os.path.splitext(img)[1]
 
             if ext != ".gif":
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(img) as resp:
+                        if resp.status == 200:
+
+                            # Get values for deep fry
+                            try:
+                                saturation_val = int(os.environ.get("FRY_SAT"))
+                                brightness_val = int(os.environ.get("FRY_BRIGHT"))
+                                contrast_val = int(os.environ.get("FRY_CONTRAST"))
+                                sharpness_val = int(os.environ.get("FRY_SHARPNESS"))
+                            except Exception as e:
+                                print(e)
+                                saturation_val = 4
+                                brightness_val = 4
+                                contrast_val = 20
+                                sharpness_val = 300
+
+                            file = await aiofiles.open("deepfried.png", mode="wb")
+                            await file.write(await resp.read())
+                            await file.close()
+
+                            # Open with PIL and "enhance" it
+                            im = Image.open("deepfried.png")
+                            saturated = ImageEnhance.Color(im).enhance(saturation_val)
+                            brightness = ImageEnhance.Brightness(saturated).enhance(brightness_val)
+                            contrast = ImageEnhance.Contrast(brightness).enhance(contrast_val)
+                            final = ImageEnhance.Sharpness(contrast).enhance(sharpness_val)
+
+                            final.save("deepfried.png", format="png")
+                            await channel.send("Fresh from the fryer!", file=discord.File("deepfried.png"))
+
+                    # Delete temp picture file
+                    os.remove("deepfried.png")
                 # Download image from URL
-                try:
-                    async with aiohttp.ClientSession() as session:
-                        async with session.get(img) as resp:
-                            if resp.status == 200:
-
-                                # Get values for deep fry
-                                try:
-                                    saturation_val = int(os.environ.get("FRY_SAT"))
-                                    brightness_val = int(os.environ.get("FRY_BRIGHT"))
-                                    contrast_val = int(os.environ.get("FRY_CONTRAST"))
-                                    sharpness_val = int(os.environ.get("FRY_SHARPNESS"))
-                                except Exception as e:
-                                    print(e)
-                                    saturation_val = 4
-                                    brightness_val = 4
-                                    contrast_val = 20
-                                    sharpness_val = 300
-
-                                file = await aiofiles.open("deepfried.png", mode="wb")
-                                await file.write(await resp.read())
-                                await file.close()
-
-                                # Open with PIL and "enhance" it
-                                im = Image.open("deepfried.png")
-                                saturated = ImageEnhance.Color(im).enhance(saturation_val)
-                                brightness = ImageEnhance.Brightness(saturated).enhance(brightness_val)
-                                contrast = ImageEnhance.Contrast(brightness).enhance(contrast_val)
-                                final = ImageEnhance.Sharpness(contrast).enhance(sharpness_val)
-
-                                final.save("deepfried.png", format="png")
-                                await channel.send("Fresh from the fryer!", file=discord.File("deepfried.png"))
-
-                        # Delete temp picture file
-                        os.remove("deepfried.png")
-                except Exception as e:
-                    print(e)
-                    await channel.send("```Found no image```")
             else:
                 async with aiohttp.ClientSession() as session:
                     async with session.get(img) as resp:
@@ -82,7 +78,7 @@ class DeepFry(commands.Cog):
                             await file.close()
 
                             if not os.path.isdir(self.download_dir): os.mkdir(self.download_dir)
-                            await channel.send(file=discord.File(
+                            await channel.send("Fresh from the fryer!", file=discord.File(
                                 await self.assemble_gif("deepfry.gif", self.download_dir)))
                             # Delete off server
                             shutil.rmtree(self.download_dir)
