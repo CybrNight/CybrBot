b603b1437eb0443bca4399aaeb3e236578330072
Nathan Estrada <naestrada19@gmail.com>
2019-08-13 23:28:22 -0700

    Update deep_fry.py

--- bot/cogs/deep_fry.py
+++ bot/cogs/deep_fry.py
@@ -1,18 +1,14 @@
 import discord
 from discord.ext import commands
-import aiofiles, aiohttp
-from PIL import Image, ImageEnhance
-import os, shutil
-from os import listdir
-from os.path import isfile, join
-import imageio
+from bot import reference as ref
+import os, json
+
+import asyncio, random
 
 class DeepFry(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.download_dir = os.getcwd()+"/download/deepfry"
-
 
 def setup(bot):
     bot.add_cog(DeepFry(bot))
