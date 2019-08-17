fd725201b0bcb4bd606aef2fd80e315638689f42
Nathan Estrada <naestrada19@gmail.com>
2019-08-16 21:00:06 -0700

    Started music playback and more PEP8 compliance

--- bot/botmain.py
+++ bot/botmain.py
@@ -10,12 +10,15 @@ bot.remove_command("help")
 cogs_dir = "cogs"
 print(f"Running Python {sys.version}")
 
+
 @bot.event
 async def on_ready():
-    #await bot.change_presence(game=Game(name="with fellow humans"))
-    if not os.path.isdir(DOWNLOAD_DIRECTORY): os.mkdir(DOWNLOAD_DIRECTORY)
-    if not os.path.isdir(JPEG_DIRECTORY): os.mkdir(JPEG_DIRECTORY)
-    if not os.path.isdir(FRY_DIRECTORY): os.mkdir(FRY_DIRECTORY)
+    if not os.path.isdir(DOWNLOAD_DIRECTORY):
+        os.mkdir(DOWNLOAD_DIRECTORY)
+    if not os.path.isdir(JPEG_DIRECTORY):
+        os.mkdir(JPEG_DIRECTORY)
+    if not os.path.isdir(FRY_DIRECTORY):
+        os.mkdir(FRY_DIRECTORY)
     print(f"Logged in as {bot.user.name}")
 
 cogs = os.listdir(cogs_dir)
@@ -24,7 +27,8 @@ if __name__ == "__main__":
     for extension in [f.replace('.py', '') for f in os.listdir(cogs_dir) if os.path.isfile(os.path.join(cogs_dir, f))]:
         try:
             bot.load_extension(f"{cogs_dir}.{extension}")
+            print(f"Loaded extension: {extension}")
         except Exception as e:
             print(e)
-            print(f'Failed to load extension {extension}.')
+            print(f'Failed to load extension: {extension}.')
     bot.run(TOKEN)
