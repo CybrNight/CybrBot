7f1c50e42b98b852f527d9c76b92db9e437c8cf8
Nathan Estrada <naestrada19@gmail.com>
2019-08-17 14:44:50 -0700

    More audio work complete

--- bot/cogs/audio.py
+++ bot/cogs/audio.py
@@ -15,7 +15,8 @@ class Voice(commands.Cog):
         self.bot = bot
         self.players = {}
         self.youtube_id = ""
-        self.queue_count = 0
+        self.queue = []
+        self.queue_index = 0
 
         self.bot.loop.create_task(self.initialize())
 
@@ -32,7 +33,7 @@ class Voice(commands.Cog):
         if voice and voice.is_connected():
             await voice.disconnect()
             print(f"Disconnected from {channel}")
-            await ctx.send(f"```Left {channel}")
+            await ctx.send(f"```Left {channel}```")
         else:
             print("Told to leave channel, but not connected to one")
 
@@ -47,7 +48,7 @@ class Voice(commands.Cog):
         else:
             voice = await channel.connect()
             print(f"Connected to {channel}\n")
-            await ctx.send(f"```Joined {channel}")
+            await ctx.send(f"```Joined {channel}```")
 
     @commands.command(pass_context=True)
     async def play(self, ctx):
@@ -63,20 +64,23 @@ class Voice(commands.Cog):
             print(f"Connected to {channel}\n")
             await ctx.send(f"```Joined {channel}```")
 
-        file = os.listdir(AUDIO_DIRECTORY)[0]
-        full_file = AUDIO_DIRECTORY+"/"+file
+        file = self.queue[self.queue_index]
+        full_file = f"{AUDIO_DIRECTORY}/{self.queue[self.queue_index]}.mp3"
 
-        voice.play(discord.FFmpegPCMAudio(full_file), after=lambda e: print(f"{full_file} has finished"))
+        voice.play(discord.FFmpegPCMAudio(full_file), after=lambda e: os.remove(full_file))
         voice.source = discord.PCMVolumeTransformer(voice.source)
         voice.source.volume = 0.07
 
         print(f"Playing {file}")
-        await ctx.send(f"Playing {file}")
+        await ctx.send(f"```Playing {file}```")
+
 
     @commands.command(pass_context=True, name="queue")
     async def queue_control(self, ctx, option="-v", url=None):
         print(option)
+        ctx.message.delete()
         if option == "-c":
+            self.queue.clear()
             for file in os.listdir(AUDIO_DIRECTORY):
                 os.remove(f"{AUDIO_DIRECTORY}/{file}")
             await ctx.send("```Cleared Queue```")
@@ -85,8 +89,8 @@ class Voice(commands.Cog):
         elif option == "-v":
             i = 1
             queue_list = ""
-            for file in os.listdir(AUDIO_DIRECTORY):
-                queue_list += f"{i}. {os.path.splitext(file)[0]}\n"
+            for song in self.queue:
+                queue_list += f"{i}. {song}\n"
                 i += 1
             await ctx.send(f"```{queue_list}```")
             return
@@ -115,10 +119,11 @@ class Voice(commands.Cog):
 
                 print(f"Added {url}")
                 await adding_msg.delete()
-                self.queue_count += 1
+                self.queue.append(video["title"])
                 embed_queue = discord.Embed(title=f"Added {video['title']}", color=0x00ff00)
-                embed_queue.set_video(url=video["thumbnail"])
-                embed_queue.add_field(name=f"{self.queue_count} song(s) in queue", value=f"/play to play queue")
+                embed_queue.set_image(url=video["thumbnail"])
+                queue_length = len(self.queue)
+                embed_queue.add_field(name=f"{queue_length} song(s) in queue", value=f"/play to play queue")
                 temp = await ctx.send(embed=embed_queue)
                 print(f"Added {video['tile']} to song queue")
             except Exception as e:
