b6810afc473c8bfaf45d08b83c8b9bcd7a1efc70
Nathan Estrada <naestrada19@gmail.com>
2019-08-18 10:07:11 -0700

    Prettify progess

--- bot/cogs/music.py
+++ bot/cogs/music.py
@@ -11,6 +11,7 @@ from bot.reference import *
 
 
 class Music(commands.Cog):
+    global voice
 
     def __init__(self, bot):
         self.bot = bot
@@ -22,6 +23,8 @@ class Music(commands.Cog):
 
     async def initialize(self):
         await self.bot.wait_until_ready()
+
+        # Load Opus library for voice
         if not discord.opus.is_loaded():
             discord.opus.load_opus('opus')
 
@@ -39,7 +42,6 @@ class Music(commands.Cog):
     @commands.command(pass_context=True)
     async def leave(self, ctx):
         channel = ctx.message.author.voice.channel
-        voice = get(self.bot.voice_clients, guild=ctx.guild)
 
         if voice and voice.is_connected():
             await voice.disconnect()
@@ -47,13 +49,15 @@ class Music(commands.Cog):
         else:
             print("Told to leave channel, but not connected to one")
 
+    # Tells bot to join text channel in
     @commands.command(pass_context=True)
     async def join(self, ctx):
+        global voice
         try:
-            global voice
             channel = ctx.message.author.voice.channel
             voice = get(self.bot.voice_clients, guild=ctx.guild)
 
+            # Connect bot to voice channel
             if voice and voice.is_connected():
                 await voice.move_to(channel)
             else:
@@ -61,29 +65,23 @@ class Music(commands.Cog):
                 print(f"Connected to {channel}\n")
         except Exception as e:
             print(e)
-            msg = await ctx.send("```Must be in voice channel to use this command```")
-            await asyncio.sleep(1.5)
-            await ctx.message.delete()
-            await msg.delete()
+            await ctx.send("**Must be in voice channel to use this command**")
 
     @commands.command(pass_context=True)
     async def play(self, ctx, url=None):
-        global voice
+        voice = get(self.bot.voice_clients, guild=ctx.guild)
+        channel = ctx.message.author.voice.channel
         if url is None and len(self.queue) > 0:
             try:
-                server = ctx.guild
-                channel = ctx.message.author.voice.channel
-                voice = get(self.bot.voice_clients, guild=ctx.guild)
+                if voice and voice.is_connected():
+                    await voice.move_to(channel)
+                else:
+                    await channel.connect()
+                    print(f"Connected to {channel}\n")
             except Exception as e:
                 print(e)
                 await ctx.send("```Must be in voice channel to use this command```")
                 return
-
-            if voice and voice.is_connected():
-                await voice.move_to(channel)
-            else:
-                voice = await channel.connect()
-                print(f"Connected to {channel}\n")
 
             self.queue_index = 0
             self.play_queue(ctx)
@@ -92,7 +90,6 @@ class Music(commands.Cog):
             try:
                 server = ctx.guild
                 channel = ctx.message.author.voice.channel
-                voice = get(self.bot.voice_clients, guild=ctx.guild)
             except Exception as e:
                 print(e)
                 msg = await ctx.send("```Must be in voice channel to use this command```")
@@ -197,11 +194,7 @@ class Music(commands.Cog):
 
             self.queue.update({title: duration})
 
-            embed_queue = await self.embed_queued(song, option)
-
-            user = ctx.message.author
-            embed_queue.set_author(name="Added to queue", icon_url=f"https://cdn.discordapp.com/avatars/"
-                                                                   f"{user.id}/{user.avatar}.png?size=1024")
+            embed_queue = await self.embed_song(song, show_queue=True, ctx=ctx)
 
             await ctx.send(embed=embed_queue)
             print(f"Added {title} to song queue")
@@ -212,7 +205,7 @@ class Music(commands.Cog):
         voice = get(self.bot.voice_clients, guild=ctx.guild)
 
         if len(self.queue) == 0:
-            self.queue_finished(ctx)
+            self.clear_queue()
         else:
             file = list(self.queue)[self.queue_index]
             full_file = f"{AUDIO_DIRECTORY}/{file}.mp3"
@@ -236,14 +229,32 @@ class Music(commands.Cog):
             if len(self.queue) > 0:
                 await self.play_queue(ctx)
             else:
-                self.queue_finished(ctx)
+                self.clear_queue()
         else:
             msg = await ctx.send(":x: **Music not playing**")
             print("No music to stop!")
 
-    def queue_finished(self, ctx):
-        print("Queue done")
-        self.clear_queue()
+    async def embed_song(self, song, show_queue=False, ctx=None):
+        title = song['title']
+        duration = str(datetime.timedelta(seconds=int(song['duration'])))
+        thumbnail = song["thumbnail"]
+        url = f"https://www.youtube.com/watch?v={song['id']}"
+        queue_length = len(self.queue)
+
+        embed = discord.Embed(title=f"**{title}**", url=url, color=0x00ff00)
+        embed.add_field(name="Duration:", value=f"{duration}", inline=False)
+        embed.set_thumbnail(url=thumbnail)
+
+        if show_queue and ctx is not None:
+            user = ctx.message.author
+            embed.add_field(name=f"**Position in queue:**", value=str(queue_length))
+            embed.set_footer(text=f"{queue_length} song(s) in queue\n /play")
+            embed.set_author(name="Added to queue", icon_url=f"https://cdn.discordapp.com/avatars/"
+                                                             f"{user.id}/{user.avatar}.png?size=1024")
+        else:
+            embed.set_author(name="Now Playing")
+
+        return embed
 
     async def embed_queued(self, song, url):
         title = song['title']
@@ -278,6 +289,7 @@ class Music(commands.Cog):
             with youtube_dl.YoutubeDL(self.ydl_opts) as ydl:
                 print("Download started")
                 result = ydl.extract_info(url, download=True)
+                print(result)
 
                 if "entries" in result:  # Add playlist support
                     video = result[0]
