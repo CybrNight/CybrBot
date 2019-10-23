ce24f272f2c84dafe5c68c12a00234bfb5aa64e4
Nathan Estrada <naestrada19@gmail.com>
2019-10-23 12:20:57 -0700

    More permissions revamp

--- bot/reference.py
+++ bot/reference.py
@@ -11,6 +11,7 @@ AUDIO_DIRECTORY = os.getcwd() + "/resources/audio"
 DOWNLOAD_DIRECTORY = os.getcwd()+"/download"
 PRESENCE_JSON = os.getcwd()+"/resources/json/presence.json"
 COMMAND_JSON = os.getcwd()+"/resources/json/commands.json"
+ALLOWED_CHANNELS_JSON = os.getcwd()+"resources/json/allowed_channels.json"
 
 RELEASE_VERSION = "3.0"
 
@@ -29,33 +30,44 @@ ROLES = {
 
 async def check_can_use(ctx, command=None):
     try:
+        with open(ALLOWED_CHANNELS_JSON, "r") as channels:
+            channel = json.load(channels)
+    except Exception as e:
+        print("Failed to load allowed_channels.json")
+
+    try:
         with open(COMMAND_JSON, "r") as cmds:
             cmd = json.load(cmds)
-            can_use = False
-            # Check if commands exist
-            for index, item in enumerate(cmd["commands"]):
-                if item["name"] == command:
-                    for role in ctx.author.roles:
-                        if role.name in ROLES:
-                            if ROLES[role.name] >= int(item["permission-level"]):
-                                can_use = True
-
-            if ctx.author.id == 229773126936821760:
-                can_use = True
-
-            if ctx.message.channel.name not in ALLOWED_CHANNELS:
-                print(f"Not in bot-commands, but actually in {ctx.message.channel.name}")
-                can_use = False
-
-            cmds.close()
-
-            if not can_use:
-                await ctx.message.delete()
-
-            return can_use
     except Exception as e:
         print("Failed to load command.json")
         print(e)
+
+    can_use = False
+    # Check if commands exist
+    for index, item in enumerate(cmd["commands"]):
+        if item["name"] == command:
+            for role in ctx.author.roles:
+                if role.name in ROLES:
+                    if ROLES[role.name] >= int(item["permission-level"]):
+                        can_use = True
+
+    if ctx.author.id == 229773126936821760:
+        can_use = True
+
+    for index, item in enumerate(channel["allowed-channels"]):
+        if item["name"] == ctx.message.channel.name:
+            can_use = True
+            break
+        else:
+            can_use = False
+
+    channels.close()
+    cmds.close()
+
+    if not can_use:
+        await ctx.message.delete()
+
+    return can_use
 
 try:
     BOT_PREFIX = os.environ["BOT_PREFIX"]
