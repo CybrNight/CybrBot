3174bc3fa709d42451e3e6fafe31ece90c083c14
Nathan Estrada <naestrada19@gmail.com>
2020-01-25 21:41:25 -0800

    Basic claiming framework in place

--- bot/cogs/waifu.py
+++ bot/cogs/waifu.py
@@ -20,8 +20,11 @@ class Waifu(commands.Cog):
         if path.isfile(WISHLIST_DIRECTORY+"/"+author_name+".csv"):
             print("You have a wishlist")
         else:
-            f = open(WISHLIST_DIRECTORY+"/"+author_name+".csv", "w+")
-            f.close()
+            try:
+                f = open(WISHLIST_DIRECTORY+"/"+author_name+".csv", "w+")
+                f.close()
+            except Exception:
+                print("Error creating csv wishlist file for "+author_name)
 
     @commands.command(name="wishlist", aliases=["wl"])
     async def wishlist(self, ctx):
@@ -30,8 +33,11 @@ class Waifu(commands.Cog):
         if path.isfile(WISHLIST_DIRECTORY+"/"+author_name+".csv"):
             print("You have a wishlist")
         else:
-            f = open(WISHLIST_DIRECTORY+"/"+author_name+".csv", "w+")
-            f.close()
+            try:
+                f = open(WISHLIST_DIRECTORY + "/" + author_name + ".csv", "w+")
+                f.close()
+            except Exception:
+                print("Error creating csv wishlist file for " + author_name)
 
     @commands.command(name="w")
     async def waifu(self, ctx):
@@ -44,18 +50,31 @@ class Waifu(commands.Cog):
             with open(file, "r", encoding="utf8",
                       errors="ignore") as json_file:
                 self.waifu_json = json.load(json_file)
-        except FileNotFoundError:
+        except Exception:
             print("No Such File")
 
         waifu = random.choice(self.waifu_json["characters"])
         name = waifu["name"]
-        image = waifu["image"]
+        image = waifu["images"][0]
 
         embed = discord.Embed(title=f"**{name}**", color=0xffdd00)
         embed.add_field(name=f"{self.waifu_json['name']}", value="\u200b")
         embed.set_image(url=image)
 
-        await ctx.send(embed=embed)
+        message = await ctx.send(embed=embed)
+        await message.add_reaction("💖")
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        user = payload.member
+        emoji = payload.emoji
+
+        if user.id == self.bot.user.id:
+            return
+
+        if str(emoji) == "💖":
+            print(f"{user} claimed a character")
+
 
 def setup(bot):
     bot.add_cog(Waifu(bot))
