0e883f3efa58000bda1be28f8ca07aed0822fc3b
Nathan Estrada <naestrada19@gmail.com>
2025-07-20 22:22:01 -0700

    Update media.py

--- bot/cogs/media.py
+++ bot/cogs/media.py
@@ -5,8 +5,6 @@ from discord.ext import commands
 
 from bot.reference import *
 
-hh = HentaiHaven()
-ph = PornHub()
 r34 = Rule34()
 gb = Gelbooru()
 
@@ -21,16 +19,6 @@ class Media(commands.Cog):
         em.set_image(url=img)
         return em
 
-    @commands.command(name='hentaihaven', aliases=['hh', 'hentai'])
-    async def hentai_haven(self, ctx, *args):
-        can_send = await check_can_use(ctx, "hentaihaven")
-        if not can_send:
-            return
-
-        video = await hh.category_search(str(" ").join(args))
-        await ctx.channel.send(video)
-        return
-        
     @commands.command(name='r34', alias=['rule34'])
     async def rule34(self, ctx, *args):
         can_send = await check_can_use(ctx, "rule34")
